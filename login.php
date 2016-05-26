aee7c49057db4f82f41f103d6e8f63e7fd294ae9
Ratchanan Srirattanamet <peathot@hotmail.com>
2016-05-26 17:23:42 +0700

    Change method to set UAM_URL

--- login.php
+++ login.php
@@ -46,14 +46,6 @@
 require_once 'config.php';
 require_once 'functions.php';
 
-function readUamIPAndPort() {
-	global $config;
-
-	if (isset($_REQUEST['uamip']) && isset($_REQUEST['uamport'])) {
-		$config['UAM_URL']='http://'.$_REQUEST['uamip'].':'.$_REQUEST['uamport'];
-	}
-}
-
 function handleUsernamePasswordLogin() {
 	global $config;
 
@@ -181,7 +173,10 @@ function redirectToLoginPage() {
 	goToFile("./loginForm.php");
 }
 
-readUamIPAndPort();
+//Read uamip and uamport
+if (isset($_REQUEST['uamip']) && isset($_REQUEST['uamport'])) {
+	$config['UAM_URL']='http://'.$_REQUEST['uamip'].':'.$_REQUEST['uamport'];
+}
 
 //Handle login
 if ($_REQUEST['res'] == 'wispr') {
